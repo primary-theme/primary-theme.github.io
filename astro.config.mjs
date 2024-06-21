40031c199c0816753ebe50686285a5eac0c465bc
Cecilia May <90643719+ceciliamay@users.noreply.github.com>
2024-06-21 13:27:04 +0800

    restart

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://primary-theme.github.io',
 	integrations: [
 		starlight({
 			title: 'Primary Theme',
